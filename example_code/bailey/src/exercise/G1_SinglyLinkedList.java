28d036c0b2860ed6151d2f351a6a5e48b7c08f83
Seke <sekewei@users.noreply.github.com>
2024-03-06 12:54:42 +0800

    Update G1_SinglyLinkedList.java

--- example_code/bailey/src/exercise/G1_SinglyLinkedList.java
+++ example_code/bailey/src/exercise/G1_SinglyLinkedList.java
@@ -1,5 +1,27 @@
 /*
- */
+* G1_SinglyLinkedList.java
+*   單鍵結單向鏈結清單內含長度、首節點、尾節點三欄位，
+*   添加首元素和尾元素的測試場景。
+*
+* > java G1_SinglyLinkedList
+*/
+/*
+G1分組練習: 單向鏈結清單 W3~W4. 3/13 (三)
+
+課本單向鏈結清單內含長度及首節點兩欄位，
+其設計不利於尾部增刪元素運算。
+請為其變更設計，增加尾節點欄位。
+評估在此首尾節點設計下，清單新增節點效能。
+以分別添加首尾元素1百萬次為例,
+列印新增首元素及尾元素的平均時間。
+
+補充題:
+1.為清單撰寫是否包含某元素方法contains
+2.為清單撰寫特定位置查改元素方法get,set
+3.為清單撰寫首尾找元素方法indexOf,lastIndexOf
+4.為清單撰寫刪除首元素,尾元素方法removeFirst,removeLast
+5.為清單撰寫特定位置增刪元素方法add,remove
+*/
 package exercise;
 public class G1_SinglyLinkedList
 {
